bdf880b14bb2d953453735355c1fa6dfc44759ed
cf <abtjh5q@hmamail.com>
2018-12-23 15:49:53 +0100

    Add autogenerated parcelable implementation for host object

--- app/src/main/java/de/fuchsch/ws2812mirrorcontrol/model/Host.kt
+++ app/src/main/java/de/fuchsch/ws2812mirrorcontrol/model/Host.kt
@@ -1,3 +1,31 @@
 package de.fuchsch.ws2812mirrorcontrol.model
 
-data class Host(val host: String, val ip: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Host(val host: String, val ip: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(host)
+        parcel.writeString(ip)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Host> {
+        override fun createFromParcel(parcel: Parcel): Host {
+            return Host(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Host?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
