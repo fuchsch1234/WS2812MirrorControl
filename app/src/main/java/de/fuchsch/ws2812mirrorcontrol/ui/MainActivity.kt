581fef9f40d971f2adbfed49b7a36401ab67035d
cf <abtjh5q@hmamail.com>
2018-12-23 16:15:01 +0100

    Add second activity to control the ws2812 mc

--- app/src/main/java/de/fuchsch/ws2812mirrorcontrol/ui/MainActivity.kt
+++ app/src/main/java/de/fuchsch/ws2812mirrorcontrol/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package de.fuchsch.ws2812mirrorcontrol.ui
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import de.fuchsch.ws2812mirrorcontrol.R
+import de.fuchsch.ws2812mirrorcontrol.model.Host
 import de.fuchsch.ws2812mirrorcontrol.viewmodel.SelectedHostViewModel
 
 class MainActivity : FragmentActivity() {
@@ -14,5 +17,17 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         selectedHostViewModel = ViewModelProviders.of(this).get(SelectedHostViewModel::class.java)
+        selectedHostViewModel.selectedHost.observe(this, Observer { if (it != null) displayHost(it) })
+    }
+
+    private fun displayHost(host: Host) {
+        val intent = Intent(this, WS2812Activity::class.java).apply {
+            putExtra(HOST, host)
+        }
+        startActivity(intent)
+    }
+
+    companion object {
+        const val HOST = "de.fuchsch.ws2812mirrorcontrol.HOST"
     }
 }
---
+++ app/src/main/java/de/fuchsch/ws2812mirrorcontrol/ui/WS2812Activity.kt
@@ -0,0 +1,19 @@
+package de.fuchsch.ws2812mirrorcontrol.ui
+
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import de.fuchsch.ws2812mirrorcontrol.R
+import de.fuchsch.ws2812mirrorcontrol.viewmodel.SelectedHostViewModel
+
+class WS2812Activity : AppCompatActivity() {
+
+    private lateinit var selectedHostViewModel: SelectedHostViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ws2812)
+        selectedHostViewModel = ViewModelProviders.of(this).get(SelectedHostViewModel::class.java)
+        selectedHostViewModel.selectedHost.value = intent.getParcelableExtra(MainActivity.HOST)
+    }
+}
