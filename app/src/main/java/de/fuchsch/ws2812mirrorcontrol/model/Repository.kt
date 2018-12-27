efcc93f682df18703e67f7a1a5785e0c3c0c1bc8
cf <abtjh5q@hmamail.com>
2018-12-27 11:42:32 +0100

    Retrieving list of effects from device through repository works

--- app/src/main/java/de/fuchsch/ws2812mirrorcontrol/model/Repository.kt
+++ app/src/main/java/de/fuchsch/ws2812mirrorcontrol/model/Repository.kt
@@ -5,7 +5,12 @@ import de.fuchsch.ws2812mirrorcontrol.injection.RepositoryInjector
 import de.fuchsch.ws2812mirrorcontrol.injection.RepositoryModule
 import de.fuchsch.ws2812mirrorcontrol.network.BaseUrlHolder
 import de.fuchsch.ws2812mirrorcontrol.network.WS2812Api
-import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.logging.Logger
 import javax.inject.Inject
 
 class Repository {
@@ -16,18 +21,36 @@ class Repository {
     @Inject
     lateinit var baseUrlHolder: BaseUrlHolder
 
-    private val injector: RepositoryInjector = DaggerRepositoryInjector
-        .builder()
-        .repositoryModule(RepositoryModule)
-        .build()
+    val effects: PublishSubject<EffectList> = PublishSubject.create()
+
+    private var disposable: Disposable? = null
+
+    private val logger: Logger = Logger.getLogger(Repository::class.java.canonicalName)
 
     init {
+        // Set a default error handler that swallows all error messages
+        // Necessary to use effects observable without an attached observer
+        RxJavaPlugins.setErrorHandler { }
         injector.inject(this)
+        retrieveEffectList()
     }
 
     fun changeHost(host: Host) {
-        baseUrlHolder.baseUrl = "http://" + host.ip
+        baseUrlHolder.baseUrl = "http://${host.ip}/api/v1/"
+        logger.info("Host changed to ${baseUrlHolder.baseUrl}")
         injector.inject(this)
+        retrieveEffectList()
+    }
+
+    private fun retrieveEffectList() {
+        disposable?.dispose()
+        disposable = ws2812Api.getEffects()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { effects.onNext(it) },
+                { effects.onError(it) }
+            )
     }
 
     companion object {
